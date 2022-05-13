47b80b867a7798a9a2d8dabe498c401ae3cb9815
E. Earl <elizabethsanch95@gmail.com>
2022-05-13 18:04:24 +0100

    Update need.php

--- need.php
+++ need.php
@@ -1,5 +1,28 @@
 <html>
 <body>
-Do we need it: <?php echo $color; ?><br>
+<?php
+
+$selected_colors = filter_input(
+	INPUT_POST,
+	'colors',
+	FILTER_SANITIZE_STRING,
+	FILTER_REQUIRE_ARRAY
+);
+?>
+
+<?php if ($selected_colors) : ?>
+	<p>You selected the following colors:</p>
+	<ul>
+		<?php foreach ($selected_colors as $color) : ?>
+			<li style="color:<?php echo $color ?>"><?php echo $color ?></li>
+		<?php endforeach ?>
+	</ul>
+	<p>
+
+	</p>
+
+<?php else : ?>
+	<p>You did not select any color.</p>
+<?php endif ?>
 </body>
 </html>
